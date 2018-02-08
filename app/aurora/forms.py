91b0e88650b62afa8450c7fa0db78908587049a2
Arik Sidney Guggenheim <ariksidney@outlook.com>
2018-02-08 16:13:05 +0100

    added default port

--- app/aurora/forms.py
+++ app/aurora/forms.py
@@ -7,7 +7,7 @@ from ..models import User
 class AddAuroraForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired(), Length(1, 64)])
     ip = StringField('IP Address', validators=[DataRequired()])
-    port = IntegerField('Port', validators=[DataRequired()])
+    port = IntegerField('Port', validators=[DataRequired()], default="16021")
     token = StringField('Token')
     submit = SubmitField('Save')
     #  TODO add ip regex
